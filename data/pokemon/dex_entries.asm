fa15e8e1a53492f6a9b22a34570814e6547a2d55
clodsire74 <davidstormking0@gmail.com>
2025-02-13 11:27:02 -0800

    Update dex_entries.asm

--- data/pokemon/dex_entries.asm
+++ data/pokemon/dex_entries.asm
@@ -30,7 +30,7 @@ PokedexEntryPointers:
 	dw BlastoiseDexEntry
 	dw PinsirDexEntry
 	dw TangelaDexEntry
-	dw MissingNoDexEntry
+	dw QuaquavalDexEntry
 	dw MissingNoDexEntry
 	dw GrowlitheDexEntry
 	dw OnixDexEntry
@@ -405,6 +405,13 @@ TangelaDexEntry:
 	db 3,3
 	dw 770
 	text_far _TangelaDexEntry
+	text_end
+
+QuaquavalDexEntry:
+	db "DANCER@"
+	db 5,11
+	dw 1370
+	text_far _QuaquavalDexEntry
 	text_end
 
 GrowlitheDexEntry:
