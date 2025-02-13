0f366296e040781f7d7c06c3971cc873e00c38bb
clodsire74 <davidstormking0@gmail.com>
2025-02-12 17:26:25 -0800

    Update evos_moves.asm

--- data/pokemon/evos_moves.asm
+++ data/pokemon/evos_moves.asm
@@ -40,7 +40,7 @@ EvosMovesPointerTable:
 	dw BlastoiseEvosMoves
 	dw PinsirEvosMoves
 	dw TangelaEvosMoves
-	dw MissingNo1FEvosMoves
+	dw QuaquavalEvosMoves
 	dw MissingNo20EvosMoves
 	dw GrowlitheEvosMoves
 	dw OnixEvosMoves
